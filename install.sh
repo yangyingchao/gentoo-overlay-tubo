638e72738aae7d9bee37a7edfc7475e6ff8993f9
yangyingchao <yang.yingchao@qq.com>
2024-04-07 09:56:24 +0800

    auto
    remove gentoo-source-6.6.18

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 #
-pushd `git rev-parse --show-toplevel`
+pushd "$(git rev-parse --show-toplevel)"
 
 for hook in post-merge pre-commit; do
-    [[ -f .git/hooks/$hook ]] || ln -sf ${PWD}/scripts/${hook} .git/hooks/$hook
+    [[ -f .git/hooks/$hook ]] || ln -sf "${PWD}"/scripts/${hook} .git/hooks/$hook
 done
 
 ./scripts/post-merge
