b1041656ec3f8ba3db762ea4c239ecf53bb19cc3
yangyingchao <yang.yingchao@qq.com>
2024-06-04 14:03:19 +0800

    update kernel

--- scripts/cleanup_manifest.sh
+++ scripts/cleanup_manifest.sh
@@ -2,13 +2,13 @@
 #
 # Author: Yang,Ying-chao <yangyingchao@g-data.com>, 2017-05-09
 #
-top=`git rev-parse --show-toplevel`
+top=$(git rev-parse --show-toplevel)
 
 cd $top
-for fn in `find $top -name "*.ebuild"`; do
+for fn in $(find $top -name "*.ebuild"); do
     echo "Processing $fn"
-    dname=`dirname $fn`
-    bname=`basename $fn`
+    dname=$(dirname $fn)
+    bname=$(basename $fn)
     cd $dname
     # sed -i "s/KEYWORDS=.*$/KEYWORDS=\"amd64 x86 amd-linux x86-linux x64-macos\"/g" $bname
     ebuild $bname digest
