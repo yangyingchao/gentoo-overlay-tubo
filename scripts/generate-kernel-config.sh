678bd1ddf35400596739febdafee847e6cdfcb4f
yangyingchao <yang.yingchao@qq.com>
2026-05-12 21:57:41 +0800

    auto commit

--- scripts/generate-kernel-config.sh
+++ scripts/generate-kernel-config.sh
@@ -1,34 +1,36 @@
 #!/usr/bin/env bash
 
-set -eo pipefail # Exit immediately if a command exits with a non-zero status.
+set -Eeuo pipefail
 
-pushd "$(git rev-parse --show-toplevel)"
+(
+    cd "$(git rev-parse --show-toplevel)"
 
-printf "\nBackup kernel config...\n"
+    printf "\nBackup kernel config...\n"
 
-CPU=$(LANG=C lscpu | grep -i 'vendor' | awk -F ':' '{print $2}' | xargs)
-VER=$(uname -r | awk -F '.' '{print $1"."$2}')
+    CPU=$(LANG=C lscpu | grep -i 'vendor' | awk -F ':' '{print $2}' | xargs)
 
-config_dir=profiles/kernel.conf.d
-config_file=
-config_files=()
+    config_dir=profiles/kernel.conf.d
+    config_files=()
 
-mkdir -p "${config_dir}"
+    mkdir -p "${config_dir}"
 
-while IFS= read -r -d '' fn; do
-    if [[ "$fn" =~ .old ]]; then
-        echo "Skip: $fn"
-        continue
-    fi
-    printf "Proc: %s" "$fn"
-    ver=$(echo "$fn" | sed -E 's/.*config-([0-9]+).([0-9]+).*/\1.\2/g')
-    config_file=$config_dir/kernel-config-${ver}-$CPU
-    printf " ==> %s," "$config_file"
+    while IFS= read -r -d '' fn; do
+        if [[ "$fn" == *.old ]]; then
+            echo "Skip: $fn"
+            continue
+        fi
+        printf "Proc: %s" "$fn"
+        ver=$(echo "$fn" | sed -E 's/.*config-([0-9]+).([0-9]+).*/\1.\2/g')
+        config_file=$config_dir/kernel-config-${ver}-$CPU
+        printf " ==> %s," "$config_file"
 
-    sed -E "/^CONFIG_(AS|GCC|CLANG|CC|LD|LLD)_VERSION/d" "$fn" > "${config_file}"
-    printf " done...\n"
-    config_files+=("$config_file")
-done < <(find /boot -name "config-*" -print0)
+        sed -E -e "/^CONFIG_(AS|GCC|CLANG|CC|LD|LLD)_VERSION/d" \
+            -e "/^# Linux.* Kernel Configuration$/d" \
+            "$fn" >"${config_file}"
+        printf " done...\n"
+        config_files+=("$config_file")
+    done < <(find /boot -name "config-*" -print0)
 
-printf "\nAdd to stage area: %s....\n" "${config_files[*]}"
-git add "${config_files[@]}" portage
+    printf "\nAdd to stage area: %s....\n" "${config_files[*]}"
+    git add "${config_files[@]}" portage
+)
